b23b91b6ee732bb3db8fea14d53c7698a0e04bf9
Yang Johnson <Yangjhnsn@outlook.com>
2024-12-02 12:28:21 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function Experience() {
+  return (
+    <>
+      <h3>Software Developer</h3>
+      <h4>Sigma Lab Sdn Bhd</h4>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <h1>Yang Johnson</h1>
+      <h2>Software Developer</h2>
+      <Experience />
+    </>
+  )
+}
+
+export default App;
